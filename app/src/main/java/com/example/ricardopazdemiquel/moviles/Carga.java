99d63fa019b7fcfadfdea6722a3961ae6587a63c
Edson Gonzalo Mamani Veizaga <37853995+edsoncito@users.noreply.github.com>
2018-08-28 17:04:21 -0400

    uj

--- app/src/main/java/com/example/ricardopazdemiquel/moviles/Carga.java
+++ app/src/main/java/com/example/ricardopazdemiquel/moviles/Carga.java
@@ -13,6 +13,8 @@ import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.google.firebase.iid.FirebaseInstanceId;
 
@@ -31,6 +33,8 @@ public class Carga extends AppCompatActivity {
     private JSONObject usr_log;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_carga);
         Token.currentToken = FirebaseInstanceId.getInstance().getToken();
@@ -75,7 +79,7 @@ public class Carga extends AppCompatActivity {
                 finish();
             }
         }
-    }, 1000);
+    }, 3000);
 
 }
     @Override
